f6829cd668631804b8f3f120eb9b732e4a1d17fe
fadelsaad99 <118170848+fadelsaad99@users.noreply.github.com>
2022-12-15 21:13:29 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,17 @@
+#include "lib.h"
 
+bool funzione(char arr2[10][20], char arr1[20],int &posizione){
+    for(int i=0; i<10; i++){
+        for(int j=0; j<20; j++){
+            if(arr2[i][j]!=arr1[j]){
+                j=20;
+            }else{
+                if(j==19){
+                    posizione=i;
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
+}
